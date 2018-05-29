d4f82ba23e7616851ab23c1c71a6c3a71e26fc9b
Deathworld <corentin.g@gmail.com>
2018-05-29 08:52:11 +0000

    Reverted last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,44 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my awesome app';
 
+  isAuth = false;
+  lastUpdate = new Date();
+
+/*  appareilOne = 'Machine à laver';
+  appareilTwo = 'Machine à refroidir';
+  appareilThree = 'Gazinière à propulsion';*/
+
+
+  /* Array d'appareils */
+
+  appareils = [
+    {
+      name: 'Machine à voyager dans le temps',
+      status: 'éteint'
+    },
+
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+
+    {
+      name: 'Machine à refroidir',
+      status: 'loin'
+    }
+  ];
+
+  /*  Attends 4 secondes au chargement de la page */
+  constructor(){
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+
+  }
+
+  onEnable(){
+    console.log('On allume tout mon pote !');
+  }
 }
