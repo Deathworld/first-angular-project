4ab74a66d866697f774731e8f7296e7e1122646e
Deathworld <corentin.g@gmail.com>
2018-06-01 18:20:56 +0000

    Organized code for rooting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,42 +11,9 @@ import {AppareilService} from './services/appareil.service';
 export class AppComponent implements OnInit {
   title = 'my awesome app';
 
-  appareils: any[];
 
-  isAuth = true;
-  lastUpdate = new Date();
-
-
-
-  // Importe AppareilService
-  constructor(private appareilService : AppareilService){
-    /*  Attend 4 secondes au chargement de la page */
-    /*      setTimeout(
-            () => {
-              this.isAuth = true;
-            }, 4000
-          );*/
-  }
+  constructor(){}
 
   ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onEnable(){
-    console.log('On allume tout mon pote !');
-    // On appelle la fonction permettant de tout allumer
-    this.appareilService.switchOnAll();
-  }
-
-  onDisable(){
-    console.log('On éteint tout mon pote !');
-
-    // Popup en haut du navigateur qui demande de confirmer
-    if(confirm('Veuillez confirmer')) {
-      // On appelle la fonction permettant de tout éteindre
-      this.appareilService.switchOffAll();
-    } else{
-      return null;
-    }
   }
 }
---
+++ src/app/appareil-view/appareil-view.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {AppareilService} from '../services/appareil.service';
+
+
+@Component({
+  selector: 'app-appareil-view',
+  templateUrl: './appareil-view.component.html',
+  styleUrls: ['./appareil-view.component.css']
+})
+export class AppareilViewComponent implements OnInit {
+
+  appareils: any[];
+
+
+
+
+  constructor(private appareilService : AppareilService){
+    /*  Attend 4 secondes au chargement de la page */
+    /*      setTimeout(
+            () => {
+              this.isAuth = true;
+            }, 4000
+          );*/
+  }
+
+  ngOnInit(){
+    this.appareils = this.appareilService.appareils;
+  }
+
+
+  onEnable(){
+    console.log('On allume tout mon pote !');
+    // On appelle la fonction permettant de tout allumer
+    this.appareilService.switchOnAll();
+  }
+
+  onDisable(){
+    console.log('On éteint tout mon pote !');
+
+    // Popup en haut du navigateur qui demande de confirmer
+    if(confirm('Veuillez confirmer')) {
+      // On appelle la fonction permettant de tout éteindre
+      this.appareilService.switchOffAll();
+    } else{
+      return null;
+    }
+  }
+}
